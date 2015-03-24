802862149267261aee2681dbf71ec3c4a3b18e88
Nithi Maniam <n.maniam1990@gmail.com>
2015-03-24 14:59:18 +0000

    Initial Commit

---
+++ app/src/main/java/com/example/nithi/interactivestory/model/Choice.java
@@ -0,0 +1,31 @@
+package com.example.nithi.interactivestory.model;
+
+/**
+ * Created by nithi on 2/22/15.
+ */
+public class Choice {
+    private String mText;
+    private int mNextPage;
+
+    public Choice(String text, int nextPage){
+        mText = text;
+        mNextPage = nextPage;
+
+    }
+
+    public String getText() {
+        return mText;
+    }
+
+    public void setText(String text) {
+        mText = text;
+    }
+
+    public int getNextPage() {
+        return mNextPage;
+    }
+
+    public void setNextPage(int nextPage) {
+        mNextPage = nextPage;
+    }
+}
